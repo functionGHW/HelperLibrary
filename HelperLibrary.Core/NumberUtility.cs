c11e3f926a7955d49c8dd6d13baf8874208ccf46
functionghw <functionghw@hotmail.com>
2015-03-11 10:34:11 +0800

    add new class NumberUtility

---
+++ HelperLibrary.Core/NumberUtility.cs
@@ -0,0 +1,89 @@
+﻿/* 
+ * FileName:    NumberUtility.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  3/11/2015 10:26:19 AM
+ * Version:     v1.0
+ * Description:
+ * */
+
+namespace HelperLibrary.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class NumberUtility
+    {
+        #region Fields
+
+        private static Lazy<Random> randLazy = new Lazy<Random>();
+
+        #endregion
+
+
+        #region Properties
+
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Convert byte array to hex format string
+        /// </summary>
+        /// <param name="bytes">the array</param>
+        /// <param name="useLowerCase"></param>
+        /// <returns>return the string of all byte with hex format
+        /// if no byte to convert, return empty string</returns>
+        /// <exception cref="ArgumentNullException">bytes is null</exception>
+        public static string BytesToHexString(byte[] bytes, bool useLowerCase = false)
+        {
+            if (bytes == null)
+            {
+                throw new ArgumentNullException("bytes");
+            }
+            int len = bytes.Length;
+            if (bytes.Length == 0)
+            {
+                return String.Empty;
+            }
+
+            Contract.Assert(len > 0);
+            char[] charAry = new char[len * 2];
+            int hexCharBase = ((useLowerCase ? 'a' : 'A') - 10);
+            for (int i = 0; i < len; i++)
+            {
+                int b = bytes[i];
+                int charPos = i * 2;
+
+                // get higher 4 bits of byte
+                int hexValue = (b & 0x000000F0) >> 4;
+                charAry[charPos] = (char)(hexValue + ((hexValue < 10) ? '0' : hexCharBase));
+
+                // get lower 4 bits of byte
+                hexValue = b & 0x0000000F;
+                charAry[charPos + 1] = (char)(hexValue + ((hexValue < 10) ? '0' : hexCharBase));
+            }
+            return new String(charAry);
+        }
+
+        /// <summary>
+        /// a static method to get a random Int32 value between [minValue, maxValue).
+        /// </summary>
+        /// <param name="minValue">min value, default value is 0</param>
+        /// <param name="maxValue">max value, default value is Int32.MaxValue</param>
+        /// <returns>a random Int32 value</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">the minValue is greater than maxValue</exception>
+        public static int GetRandomInt(int minValue = 0, int maxValue = Int32.MaxValue)
+        {
+            Contract.Requires(randLazy != null);
+            return randLazy.Value.Next(minValue, maxValue);
+        }
+
+
+        #endregion
+    }
+}
