6ed69873053b392a42f1e769ff716b754c1a01ef
functionghw <functionghw@hotmail.com>
2015-03-11 10:44:58 +0800

    test NumberUtility

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HelperLibrary.Core;
 
 namespace ConsoleTestApp
 {
@@ -17,6 +18,26 @@ namespace ConsoleTestApp
     {
         static void Main(string[] args)
         {
+
+            NumberUtilityTest();
+
+            Console.ReadKey();
+        }
+
+
+        private static void NumberUtilityTest()
+        {
+            // random int
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("random number:" + NumberUtility.GetRandomInt(-128, 127).ToString());
+            }
+
+
+            // hex string
+            byte[] bytes = new byte[] { 20, 56, 9, 10, 11, 15, 16, 56 };
+            Console.WriteLine("hex string:" + NumberUtility.BytesToHexString(bytes, true));
+
         }
     }
 }
