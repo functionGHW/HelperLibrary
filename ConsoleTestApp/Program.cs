1271ade5cc988fa1b8338a9a963bfb8defbf1200
functionghw <functionghw@hotmail.com>
2015-03-11 10:20:15 +0800

    add projets for testing libs

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+ * FileName:    Program.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  3/11/2015 10:19:04 AM
+ * Version:     v1.0
+ * Description:
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
