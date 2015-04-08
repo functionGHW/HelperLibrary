d59758bda0626a8711f7c8ef85497942f0c23f8c
functionghw <functionghw@hotmail.com>
2015-04-08 15:00:27 +0800

    Add new interface IWindowProperty;

---
+++ HelperLibrary.WPF/ExtensionInterfaces/IWindowProperty.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * FileName:    IWindowProperty.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  4/8/2015 11:38:10 AM
+ * Version:     v1.0
+ * Description:
+ * */
+
+namespace HelperLibrary.WPF.ExtensionInterfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This interface is used to ensure ViewModel class hold a property of System.Windows.Window,
+    /// which it may associate with.
+    /// </summary>
+    public interface IWindowProperty
+    {
+        /// <summary>
+        /// Gets or sets System.Windows.Window which the instance associate with
+        /// </summary>
+        System.Windows.Window Window { get; set; }
+    }
+}
