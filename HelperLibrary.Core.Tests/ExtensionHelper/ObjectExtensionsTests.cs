7268610d7d9f8a6489e3e51b1ec3f5e0fc66cb53
functionghw <functionghw@hotmail.com>
2015-06-27 13:51:44 +0800

    update test cases;

---
+++ HelperLibrary.Core.Tests/ExtensionHelper/ObjectExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿/* 
+ * FileName:    ObjectExtensionsTests.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  6/23/2015 9:23:43 PM
+ * Version:     v1.0
+ * Description:
+ * */
+
+namespace HelperLibrary.Core.ExtensionHelper.Tests
+{
+    using HelperLibrary.Core.ExtensionHelper;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture()]
+    public class ObjectExtensionsTests
+    {
+        #region Tests For ReverseArray
+
+        [Test]
+        public void ReverseArrayNullInputTest()
+        {
+            // arrange
+            int[] ary = null;
+
+            // act && assert
+            Assert.Catch<ArgumentNullException>(
+                () => ObjectExtensions.ReverseArray(ary));
+
+            Assert.Catch<ArgumentNullException>(
+               () => ObjectExtensions.ReverseArray(ary, 0, 0));
+        }
+
+        [TestCase("", -1, 0)] // startIndex out of range
+        [TestCase("", 0, -1)] // length out of range
+        [TestCase("", 1, 0)]  // startIndex plus length out of range
+        public void ReverseArrayIndexOutOfRangeTest(string orgStr, int startIndex, int length)
+        {
+            // arrange
+            char[] ary = orgStr.ToCharArray();
+
+            // act && assert
+            Assert.Catch<IndexOutOfRangeException>(
+                () => ObjectExtensions.ReverseArray(ary, startIndex, length));
+        }
+
+        [TestCase("", "")]
+        [TestCase("A", "A")]
+        [TestCase("aB", "Ba")]
+        [TestCase("aBc", "cBa")]
+        public void ReverseArrayTest(string orgStr, string expected)
+        {
+            // arrange
+            char[] ary = orgStr.ToCharArray();
+
+            // act
+            ObjectExtensions.ReverseArray(ary);
+
+            // assert
+            for (int i = 0; i < ary.Length; i++)
+            {
+                Assert.AreEqual(expected[i], ary[i]);
+            }
+        }
+
+        [TestCase("ABCD", 0, 0, "ABCD")]
+        [TestCase("ABCD", 0, 1, "ABCD")]
+        [TestCase("ABCD", 0, 2, "BACD")]
+        [TestCase("ABCD", 0, 3, "CBAD")]
+        public void ReverseArrayWithIndexTest(string orgStr, int startIndex, int length, string expected)
+        {
+            // arrange
+            char[] ary = orgStr.ToCharArray();
+
+            // act
+            ObjectExtensions.ReverseArray(ary, startIndex, length);
+
+            // assert
+            for (int i = 0; i < ary.Length; i++)
+            {
+                Assert.AreEqual(expected[i], ary[i]);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ HelperLibrary.Core.Tests/ExtensionHelper/StringExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿/* 
+ * FileName:    StringExtensionsTests.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  6/23/2015 9:08:49 PM
+ * Version:     v1.0
+ * Description:
+ * */
+
+namespace HelperLibrary.Core.ExtensionHelper.Tests
+{
+    using HelperLibrary.Core.ExtensionHelper;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture()]
+    public class StringExtensionsTests
+    {
+        #region Tests For ReverseString
+
+        [Test]
+        public void ReverseStringNullInputTest()
+        {
+            // arrange
+            string str = null;
+
+            // act && assert
+            Assert.Catch<ArgumentNullException>(
+                () => StringExtensions.ReverseString(str));
+        }
+
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("aB", "Ba")]
+        [TestCase("Abc", "cbA")]
+        public void ReverseStringTest(string orgStr, string expected)
+        {
+            // arrange
+
+            // act
+            string result = StringExtensions.ReverseString(orgStr);
+
+            // accert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+    }
+}
