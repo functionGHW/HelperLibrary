1efb0684ae708e4e89354891416a81f1a57fe66a
functionghw <functionghw@hotmail.com>
2015-08-16 09:43:40 +0800

    modify EnumUtility; add new test cases;

--- HelperLibrary.Core.Tests/EnumUtilityTests.cs
+++ HelperLibrary.Core.Tests/EnumUtilityTests.cs
@@ -1,4 +1,6 @@
-﻿/* 
+﻿using HelperLibrary.Core;
+using NUnit.Framework;
+/* 
  * FileName:    EnumUtilityTests.cs
  * Author:      functionghw<functionghw@hotmail.com>
  * CreateTime:  7/16/2015 6:11:03 PM
@@ -17,7 +19,7 @@ namespace HelperLibrary.Core.Tests
     {
 
         #region Test Data
-        private enum TestEnum
+        public enum TestEnum
         {
             A = 1,
             B = 1,
@@ -33,7 +35,7 @@ namespace HelperLibrary.Core.Tests
         {
             // act && assert
             Assert.Catch<InvalidOperationException>(
-                () => EnumUtility.GetEnumTuples<DateTime>());
+                () => EnumUtility<DateTime>.GetEnumTuples());
         }
 
         [Test()]
@@ -41,7 +43,7 @@ namespace HelperLibrary.Core.Tests
         {
             // arrange
             // act
-            var result = EnumUtility.GetEnumTuples<TestEnum>();
+            var result = EnumUtility<TestEnum>.GetEnumTuples();
 
             // assert
             Assert.IsTrue(result.Length == 3);
@@ -49,6 +51,61 @@ namespace HelperLibrary.Core.Tests
             {
                 Assert.IsTrue(Enum.GetName(typeof(TestEnum), tuple.Item1) == tuple.Item2);
             }
+        }
+
+        #endregion
+
+        #region Tests For GetEnumValue
+
+        [Test]
+        public void GetEnumValueNullArgumentTest()
+        {
+            // act && assert
+            Assert.Catch<ArgumentNullException>(
+                () => EnumUtility<TestEnum>.GetEnumValue(null));
+
+            // act && assert
+            Assert.Catch<ArgumentNullException>(
+                () => EnumUtility<TestEnum>.GetEnumValue(""));
+        }
+
+        [TestCase("B", TestEnum.B)]
+        [TestCase("b", TestEnum.B)]
+        public void GetEnumValueTest(string enumName, TestEnum expected)
+        {
+            // act
+            TestEnum result = EnumUtility<TestEnum>.GetEnumValue(enumName);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void GetEnumValueInvalidNameTest()
+        {
+            // arrange
+            string enumName = "NoSuchAName";
+
+            // act && assert
+            Assert.Catch<InvalidOperationException>(
+                () => EnumUtility<TestEnum>.GetEnumValue(enumName));
+        }
+
+        #endregion
+
+        #region Tests For GetEnumValue
+
+        [Test()]
+        public void GetEnumNameTest()
+        {
+            // arrange
+            TestEnum value = TestEnum.B;
+
+            // act
+            string result = EnumUtility<TestEnum>.GetEnumName(value);
+
+            // assert
+            Assert.AreEqual("B", result);
         }
 
         #endregion
--- HelperLibrary.Core/EnumUtility.cs
+++ HelperLibrary.Core/EnumUtility.cs
@@ -14,25 +14,92 @@ namespace HelperLibrary.Core
     using System.Text;
     using System.Threading.Tasks;
 
-    public static class EnumUtility
+    /// <summary>
+    /// A helper class for Enum
+    /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
+    public static class EnumUtility<TEnum> where TEnum : struct, IConvertible
     {
-        public static Tuple<TEnum, string>[] GetEnumTuples<TEnum>() where TEnum : struct, IConvertible
+
+        private static readonly Dictionary<TEnum, string> NameDict = new Dictionary<TEnum, string>();
+
+        private static readonly Dictionary<string, TEnum> ValueDict = new Dictionary<string, TEnum>();
+
+        private static readonly Type EnumType = typeof(TEnum);
+
+        private static void EnsureIsEnumType()
         {
-            Type enumType = typeof(TEnum);
+            if (!EnumType.IsEnum)
+                throw new InvalidOperationException(EnumType.FullName + " is not Enum");
+        }
 
-            if (!enumType.IsEnum)
-                throw new InvalidOperationException(enumType.FullName + " is not Enum");
+        /// <summary>
+        /// Get all value-name tuples of a Enum type.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type</typeparam>
+        /// <returns>an array of Tuple</returns>
+        public static Tuple<TEnum, string>[] GetEnumTuples()
+        {
+            EnsureIsEnumType();
 
-            Array values = enumType.GetEnumValues();
+            Array values = EnumType.GetEnumValues();
 
             var result = new Tuple<TEnum, string>[values.Length];
             for (int i = 0; i < values.Length; i++)
             {
                 object value = values.GetValue(i);
-                string name = enumType.GetEnumName(value);
+                string name = EnumType.GetEnumName(value);
                 result[i] = Tuple.Create((TEnum)value, name);
             }
             return result;
+        }
+
+        /// <summary>
+        /// Helper method to get enum value by its name.
+        /// </summary>
+        /// <param name="enumName">the name of value</param>
+        /// <returns>the enum value</returns>
+        /// <exception cref="ArgumentNullException">enumName is null or empty string.</exception>
+        /// <exception cref="InvalidOperationException">enumName is invalid.</exception>
+        public static TEnum GetEnumValue(string enumName)
+        {
+            EnsureIsEnumType();
+
+            if (string.IsNullOrEmpty(enumName))
+                throw new ArgumentNullException("enumName");
+
+            string enumNameToUppper = enumName.ToUpperInvariant();
+            TEnum value;
+            if (ValueDict.TryGetValue(enumNameToUppper, out value))
+            {
+                return value;
+            }
+            if (Enum.TryParse(enumNameToUppper, true, out value))
+            {
+                ValueDict[enumNameToUppper] = value;
+                return value;
+            }
+
+            throw new InvalidOperationException("Invalid name " + enumName);
+        }
+        
+        /// <summary>
+        /// Helper method to get name of a enum value.
+        /// </summary>
+        /// <param name="value">the enum value</param>
+        /// <returns>the name of the enum value</returns>
+        public static string GetEnumName(TEnum value)
+        {
+            EnsureIsEnumType();
+
+            string name;
+            if (NameDict.TryGetValue(value, out name))
+            {
+                return name;
+            }
+            name = Enum.GetName(EnumType, value);
+            NameDict[value] = name;
+            return name;
         }
     }
 }
