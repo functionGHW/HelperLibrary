65117f67ce916c269a30cbd92975f3794a2ba6c9
functionghw <functionghw@hotmail.com>
2016-03-04 17:47:04 +0800

    add new abstraction of app settings

---
+++ HelperLibrary.Core/Configurations/IAppSettings.cs
@@ -0,0 +1,31 @@
+﻿/* 
+ * FileName:    IAppSettings.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  3/4/2016 5:37:10 PM
+ * Description:
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary.Core.Configurations
+{
+    /// <summary>
+    /// manager configurations of a application
+    /// </summary>
+    public interface IAppSettings
+    {
+        string this[string name] { get;}
+
+        string Get(string name);
+
+        void Add(string name, string value);
+
+        void Set(string name, string newValue);
+
+        void Remove(string name);
+    }
+}
---
+++ HelperLibrary.Core/Configurations/LocalAppSettings.cs
@@ -0,0 +1,44 @@
+﻿/* 
+ * FileName:    LocalAppSettings.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  3/4/2016 5:42:28 PM
+ * Description:
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary.Core.Configurations
+{
+    /// <summary>
+    /// simply wrapper of ConfigurationManager.AppSettings
+    /// </summary>
+    public class LocalAppSettings : IAppSettings
+    {
+        public string this[string name] => Get(name);
+
+        public string Get(string name)
+        {
+            return ConfigurationManager.AppSettings.Get(name);
+        }
+
+        public void Add(string name, string value)
+        {
+            ConfigurationManager.AppSettings.Add(name, value);
+        }
+
+        public void Set(string name, string newValue)
+        {
+            ConfigurationManager.AppSettings.Set(name, newValue);
+        }
+
+        public void Remove(string name)
+        {
+            ConfigurationManager.AppSettings.Remove(name);
+        }
+    }
+}
