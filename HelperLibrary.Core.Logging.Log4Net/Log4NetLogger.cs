2f6a5aa5b053a8fe17dd33039253722c81b305d9
functionghw <functionghw@hotmail.com>
2016-05-10 22:49:00 +0800

    rewrite Log4NetLogger, add new constructor.

--- HelperLibrary.Core.Logging.Log4Net/Log4NetLogger.cs
+++ HelperLibrary.Core.Logging.Log4Net/Log4NetLogger.cs
@@ -16,39 +16,60 @@ namespace HelperLibrary.Core.Logging
 {
     public class Log4NetLogger : ILogger
     {
-        private readonly ILog logger;
+        private readonly ILog infoLogger;
+        private readonly ILog debugLogger;
+        private readonly ILog warnLogger;
+        private readonly ILog errorLogger;
+        private readonly ILog fatalLogger;
 
-        public Log4NetLogger(ILog logger)
+
+        public Log4NetLogger(ILog logger) : this(logger, logger, logger, logger, logger)
         {
-            if (logger == null)
-                throw new ArgumentNullException(nameof(logger));
+        }
 
-            this.logger = logger;
+        public Log4NetLogger(ILog infoLogger, ILog debugLogger, ILog warnLogger, ILog errorLogger, ILog fatalLogger)
+        {
+            if (infoLogger == null)
+                throw new ArgumentNullException(nameof(infoLogger));
+            if (debugLogger == null)
+                throw new ArgumentNullException(nameof(debugLogger));
+            if (warnLogger == null)
+                throw new ArgumentNullException(nameof(warnLogger));
+            if (errorLogger == null)
+                throw new ArgumentNullException(nameof(errorLogger));
+            if (fatalLogger == null)
+                throw new ArgumentNullException(nameof(fatalLogger));
+
+            this.infoLogger = infoLogger;
+            this.debugLogger = debugLogger;
+            this.warnLogger = warnLogger;
+            this.errorLogger = errorLogger;
+            this.fatalLogger = fatalLogger;
         }
 
         public void Info(string message)
         {
-            logger.Info(message);
+            infoLogger.Info(message);
         }
 
         public void Debug(string message)
         {
-            logger.Debug(message);
+            debugLogger.Debug(message);
         }
 
         public void Warn(string message)
         {
-            logger.Warn(message);
+            warnLogger.Warn(message);
         }
 
         public void Error(string message)
         {
-            logger.Error(message);
+            errorLogger.Error(message);
         }
 
         public void Fatal(string message)
         {
-            logger.Fatal(message);
+            fatalLogger.Fatal(message);
         }
     }
 }
