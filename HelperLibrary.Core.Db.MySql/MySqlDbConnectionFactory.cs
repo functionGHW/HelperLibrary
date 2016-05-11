f697806d50e58d5ad1522680d2d0e9a958d855b2
functionghw <functionghw@hotmail.com>
2016-05-11 20:08:59 +0800

    add new project

---
+++ HelperLibrary.Core.Db.MySql/MySqlDbConnectionFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace HelperLibrary.Core.Db.MySql
+{
+    public class MySqlDbConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string connStr;
+
+        public MySqlDbConnectionFactory(string connStr)
+        {
+            if (connStr == null)
+                throw new ArgumentNullException(nameof(connStr));
+
+            this.connStr = connStr;
+        }
+
+        public IDbConnection CreateConnection(object param)
+        {
+            return new MySqlConnection(connStr);
+        }
+
+        public IDbDataAdapter CreateDataAdapter()
+        {
+            return new MySqlDataAdapter();
+        }
+    }
+}
