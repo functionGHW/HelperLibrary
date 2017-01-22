429c628d22771b730af02d19516f45526a6905a5
functionghw <functionghw@hotmail.com>
2017-01-22 14:34:08 +0800

    1.add example for WPF.LocalizationExtension;

---
+++ Examples/Examples.WPF.LocalizationExtension/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using HelperLibrary.Core.Localization;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace Examples.WPF.LocalizationExtension
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // 在配置文件中指定，实际应用中可以通过其他手段设置
+            string language = ConfigurationManager.AppSettings["language"];
+            if (language != null && !"default".Equals(language, StringComparison.InvariantCultureIgnoreCase))
+            {
+                CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo(language);
+            }
+
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            /*
+             * 在项目中，应该在"解决方案资源管理器"中设置文件的属性：
+             *      "复制到生成目录" 设置为 "始终复制"
+             *      "生成操作" 设置为 "无"
+             */
+            string xmlFilePath = @"Localizations\Localization.xml";
+            var xmlDocument = XDocument.Load(xmlFilePath);
+            var dictionaryFactory = new XmlLocalizationDictionaryFactory(xmlDocument);
+            ILocalizationDictionary chinese = dictionaryFactory.CreateLocalizationDictionary("zh-CN");
+            ILocalizationDictionary english = dictionaryFactory.CreateLocalizationDictionary("en-US");
+
+            var mngr = new LocalizationDictionaryManager();
+            mngr.Add(chinese);
+            mngr.Add(english);
+
+            // 设置LocalizationUtility使用的字典管理器，此静态工具类通常在代码中使用
+            LocalizationUtility.SetLocalizationDictionaryManager(mngr);
+
+            // 设置本地化wpf扩展标记使用的字典管理器，此设置仅作用于wpf界面，通常和上面的保持一致。
+
+            HelperLibrary.WPF.LocalizationExtension
+                .LocalizationExtension.SetLocalizationDictionaryManager(mngr);
+        }
+    }
+}
---
+++ Examples/Examples.WPF.LocalizationExtension/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using HelperLibrary.Core.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examples.WPF.LocalizationExtension
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = GetString("title");
+        }
+
+
+        private string GetString(string key)
+        {
+            return LocalizationUtility.GetString(key, this.GetType().Assembly.GetName().Name);
+        }
+    }
+}
