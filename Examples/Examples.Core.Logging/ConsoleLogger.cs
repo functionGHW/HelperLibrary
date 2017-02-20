27f0df9a6278e3ecd2c632cae06c3ed61530c4f1
functionghw <functionghw@hotmail.com>
2017-02-20 17:36:58 +0800

    add example for Core.Logging;

---
+++ Examples/Examples.Core.Logging/ConsoleLogger.cs
@@ -0,0 +1,46 @@
+﻿/* 
+ * FileName:    ConsoleLogger.cs
+ * Author:      functionghw<functionghw@hotmail.com>
+ * CreateTime:  2017/2/20 16:48:23
+ * Version:     v1.0
+ * Description:
+ * */
+
+using HelperLibrary.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Core.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(LogLevel level, string message)
+        {
+            var oldColor = Console.ForegroundColor;
+            switch (level)
+            {
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    break;
+                case LogLevel.Warn:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case LogLevel.Fatal:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.WriteLine("[{0}] [{1}] {2}", DateTime.Now.ToString("HH:mm:ss"), level, message);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
---
+++ Examples/Examples.Core.Logging/Program.cs
@@ -0,0 +1,47 @@
+﻿using HelperLibrary.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.Core.Logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // nothing happend
+            LogTest();
+
+            // use ConsoleLogger
+            ILogger logger = new ConsoleLogger();
+            EnvironmentContext.SetLogger(logger);
+
+            // log to Console
+            LogTest();
+
+            //// use log4net
+            //logger = new Log4NetLogger(null);
+            //EnvironmentContext.SetLogger(logger);
+
+            //// log to log4net
+            //LogTest();
+
+            Console.Write("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static void LogTest()
+        {
+            var logger = EnvironmentContext.Logger;
+
+            logger.Info("log info message");
+            logger.Debug("log debug message");
+            logger.Warn("log warn message");
+            logger.Error("log error message");
+            logger.Fatal("log fatal message");
+        }
+    }
+}
