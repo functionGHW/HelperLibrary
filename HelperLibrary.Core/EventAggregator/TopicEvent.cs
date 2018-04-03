c02df051d0cd0c8d6235308ea227d4b32ca95aac
functionghw <functionghw@hotmail.com>
2018-04-03 23:22:35 +0800

    use weak reference;

--- HelperLibrary.Core/EventAggregator/TopicEvent.cs
+++ HelperLibrary.Core/EventAggregator/TopicEvent.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,29 +18,36 @@ namespace HelperLibrary.Core.EventAggregator
 {
     public class TopicEvent<TArg> : ITopicEvent
     {
-        private readonly HashSet<Action<TArg>> subscribers = new HashSet<Action<TArg>>();
+        private readonly HashSet<DelegateItem> subscribers = new HashSet<DelegateItem>();
         private readonly object syncObj = new object();
 
         public void Publish(TArg arg, bool multithread = false)
         {
-            Action<TArg>[] ary;
+            DelegateItem[] ary;
             lock (syncObj)
             {
                 ary = subscribers.ToArray();
             }
-
-            if (multithread)
+            object[] parameters = { arg };
+            foreach (var item in ary)
             {
-                foreach (var item in ary)
+                if (item.Instance != null && item.Instance.Target == null)
                 {
-                    item.BeginInvoke(arg, null, null);
+                    lock (syncObj)
+                    {
+                        subscribers.Remove(item);
+                    }
                 }
-            }
-            else
-            {
-                foreach (var item in ary)
+                else
                 {
-                    item.Invoke(arg);
+                    if (multithread)
+                    {
+                        item.DelegateAction.BeginInvoke(item.Instance?.Target, parameters, null, null);
+                    }
+                    else
+                    {
+                        item.DelegateAction.Invoke(item.Instance?.Target, parameters);
+                    }
                 }
             }
         }
@@ -48,9 +57,16 @@ namespace HelperLibrary.Core.EventAggregator
             if (action == null)
                 throw new ArgumentNullException(nameof(action));
 
+
+            var item = new DelegateItem()
+            {
+                Instance = action.Target == null ? null : new WeakReference(action.Target),
+                Method = action.Method,
+                DelegateAction = CreateAction(action.Method)
+            };
             lock (syncObj)
             {
-                subscribers.Add(action);
+                subscribers.Add(item);
             }
             return new SubscriptionObject(this, action);
         }
@@ -62,8 +78,59 @@ namespace HelperLibrary.Core.EventAggregator
 
             lock (syncObj)
             {
-                subscribers.Remove(action);
+                var target = action.Target;
+                DelegateItem result = null;
+                if (target == null)
+                {
+                    result = subscribers.FirstOrDefault(item => item.Instance == null
+                                                                && item.Method == action.Method);
+                }
+                else
+                {
+                    result = subscribers.FirstOrDefault(item => item.Instance != null
+                                                                && item.Instance.Target == target
+                                                                && item.Method == action.Method);
+                }
+                subscribers.Remove(result);
             }
+        }
+
+        private static Action<object, object[]> CreateAction(MethodInfo method)
+        {
+            var instanceParameter = Expression.Parameter(typeof(object), "instance");
+            var parametersParameter = Expression.Parameter(typeof(object[]), "parameters");
+
+            var parameterExpressions = new List<Expression>();
+            ParameterInfo[] paramInfos = method.GetParameters();
+            for (int i = 0; i < paramInfos.Length; i++)
+            {
+                var valueObj = Expression.ArrayIndex(
+                    parametersParameter, Expression.Constant(i));
+                var valueCast = Expression.Convert(
+                    valueObj, paramInfos[i].ParameterType);
+
+                parameterExpressions.Add(valueCast);
+            }
+            Expression instanceCast = method.IsStatic
+                ? null
+                : Expression.Convert(instanceParameter, method.ReflectedType);
+
+            var methodCall = Expression.Call(instanceCast, method, parameterExpressions);
+
+            var lambda = Expression.Lambda<Action<object, object[]>>(
+                methodCall, instanceParameter, parametersParameter);
+
+            Action<object, object[]> execute = lambda.Compile();
+            return execute;
+        }
+
+        private class DelegateItem
+        {
+            public WeakReference Instance;
+
+            public MethodInfo Method;
+
+            public Action<object, object[]> DelegateAction;
         }
 
         private class SubscriptionObject : ISubscriptionObject
